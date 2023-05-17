2341951baf7b5358ebb19674e49fdf8bb5eeb510
ahmedinho11y <ahmedinho.y@gmail.com>
2023-05-17 11:40:41 +0300

    sidebar responsive now!! ;)

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,83 +1,108 @@
 <div class="sidebar">
-            <!-- ----= Done by: Ahmed Yusuf =---- -->
+      <!-- ----= Done by: Ahmed Yusuf =---- -->
 
-            <!-- <li class="search-box">
+      <!-- <li class="search-box">
                   <i class='fa fa-search icon'></i>
                   <input type="text" placeholder="Search...">
             </li> -->
-            <ul class="nav-links">
-                  <li>
-                        <a href="index.php">
-                              <i class='fa fa-th-large icon'></i>
-                              <span class="link_name">Dashboard</span>
+      <ul class="nav-links">
+            <li>
+                  <a href="index.php">
+                        <i class='fa fa-th-large icon'></i>
+                        <span class="link_name">Dashboard</span>
+                  </a>
+            </li>
+            <li class=" " id="submenu-1">
+                  <div class="iocn-link" onclick="toggleMenu1()">
+                        <a href="#">
+                              <i class='fa fa-eye icon'></i>
+                              <span class="link_name">Viewing</span>
                         </a>
-                  </li>
-                  <li class=" " id="submenu-1">
-                        <div class="iocn-link" onclick="toggleMenu1()">
+                        <i class="fa fa-angle-right arrow" aria-hidden="true"></i>
+                  </div>
+                  <ul class="sub-menu">
+                        <li class="sub-menu-title">
                               <a href="#">
-                                    <i class='fa fa-eye icon'></i>
-                                    <span class="link_name">Viewing</span>
+                                    <span class="link_name" id="#viewing">Viewing</span>
                               </a>
-                              <i class="fa fa-angle-right arrow" aria-hidden="true"></i>
-                        </div>
-                        <ul class="sub-menu">
-                              <li><a href="#">Academic Plan</a></li>
-                              <li><a href="#">Contact Details</a></li>
-                        </ul>
-                  </li>
-                  <li class=" " id="submenu-2">
-                        <div class="iocn-link" onclick="toggleMenu2()">
+                        </li>
+                        <li><a href="#">Academic Plan</a></li>
+                        <li><a href="#">Contact Details</a></li>
+                  </ul>
+            </li>
+            <li class=" " id="submenu-2">
+                  <div class="iocn-link" onclick="toggleMenu2()">
+                        <a href="#">
+                              <i class='fa fa-graduation-cap icon'></i>
+                              <span class="link_name">Academic Affairs</span>
+                        </a>
+                        <i class="fa fa-angle-right arrow" aria-hidden="true"></i>
+                  </div>
+                  <ul class="sub-menu">
+                        <li class="sub-menu-title">
                               <a href="#">
-                                    <i class='fa fa-graduation-cap icon'></i>
-                                    <span class="link_name">Academic Affairs</span>
+                                    <span class="link_name" id="#academic-affairs">Academic Affairs</span>
                               </a>
-                              <i class="fa fa-angle-right arrow" aria-hidden="true"></i>
-                        </div>
-                        <ul class="sub-menu">
-                              <li><a href="course-attendance.php">Course Attendence</a></li>
-                              <li><a href="course-registration.php">Course Registration</a></li>
-                        </ul>
-                  </li>
-                  <li class=" " id="submenu-3">
-                        <div class="iocn-link" onclick="toggleMenu3()">
+                        </li>
+                        <li><a href="course-attendance.php">Course Attendence</a></li>
+                        <li><a href="course-registration.php">Course Registration</a></li>
+                  </ul>
+            </li>
+            <li class=" " id="submenu-3">
+                  <div class="iocn-link" onclick="toggleMenu3()">
+                        <a href="#">
+                              <i class="bx bx-laptop"></i>
+                              <span class="link_name">My E-services</span>
+                        </a>
+                        <i class="fa fa-angle-right arrow" aria-hidden="true"></i>
+                  </div>
+                  <ul class="sub-menu">
+                        <li class="sub-menu-title">
                               <a href="#">
-                                    <i class="bx bx-laptop"></i>
-                                    <span class="link_name">My E-services</span>
+                                    <span class="link_name" id="#my-e-services">My E-services</span>
                               </a>
-                              <i class="fa fa-angle-right arrow" aria-hidden="true"></i>
-                        </div>
-                        <ul class="sub-menu">
-                              <li><a href="course-evaluation.php">Course Evaluation</a></li>
-                              <li><a href="gpa-simulator.php">GPA Simulator</a></li>
-                              <li><a href="gpa-calculator.php">GPA Calculator</a></li>
-                        </ul>
-                  </li>
-                  <li class=" " id="submenu-4">
-                        <div class="iocn-link" onclick="toggleMenu4()">
+                        </li>
+                        <li><a href="course-evaluation.php">Course Evaluation</a></li>
+                        <li><a href="gpa-simulator.php">GPA Simulator</a></li>
+                        <li><a href="gpa-calculator.php">GPA Calculator</a></li>
+                  </ul>
+            </li>
+            <li class=" " id="submenu-4">
+                  <div class="iocn-link" onclick="toggleMenu4()">
 
+                        <a href="#">
+                              <i class='fa fa-hand-pointer-o icon'></i>
+                              <span class="link_name">My Requests</span>
+                        </a>
+                        <i class="fa fa-angle-right arrow" aria-hidden="true"></i>
+                  </div>
+                  <ul class="sub-menu">
+                        <li class="sub-menu-title">
                               <a href="#">
-                                    <i class='fa fa-hand-pointer-o icon'></i>
                                     <span class="link_name">My Requests</span>
                               </a>
-                              <i class="fa fa-angle-right arrow" aria-hidden="true"></i>
-                        </div>
-                        <ul class="sub-menu">
-                              <li><a href="course-withdrawal.php">Course Withdrawal</a></li>
-                              <li><a href="#">Official Withdrawal</a></li>
-                        </ul>
-                  </li>
-                  <li class=" " id="submenu-5">
-                        <div class="iocn-link" onclick="toggleMenu5()">
+                        </li>
+                        <li><a href="course-withdrawal.php">Course Withdrawal</a></li>
+                        <li><a href="#">Official Withdrawal</a></li>
+                  </ul>
+            </li>
+            <li class=" " id="submenu-5">
+                  <div class="iocn-link" onclick="toggleMenu5()">
+                        <a href="#">
+                              <i class='fa fa-file-text icon'></i>
+                              <span class="link_name">Reports</span>
+                        </a>
+                        <i class="fa fa-angle-right arrow" aria-hidden="true"></i>
+                  </div>
+                  <ul class="sub-menu">
+                        <li class="sub-menu-title">
                               <a href="#">
-                                    <i class='fa fa-file-text icon'></i>
                                     <span class="link_name">Reports</span>
                               </a>
-                              <i class="fa fa-angle-right arrow" aria-hidden="true"></i>
-                        </div>
-                        <ul class="sub-menu">
-                              <li><a href="course-schedule.php">Course Schedule</a></li>
-                              <li><a href="academic-transcript.php">Academic Transcript</a></li>
-                        </ul>
-                  </li>
-            </ul>
-      </div>
+                        </li>
+                        <li><a href="course-schedule.php">Course Schedule</a></li>
+                        <li><a href="academic-transcript.php">Academic Transcript</a></li>
+                  </ul>
+            </li>
+      </ul>
+</div>
