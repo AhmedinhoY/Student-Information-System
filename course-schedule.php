9995c308db75dbe38cde62c74af8e2957facdef2
ahmedinho11y <ahmedinho.y@gmail.com>
2023-05-20 21:35:24 +0300

    course schedule page backend modified

--- course-schedule.php
+++ course-schedule.php
@@ -1,4 +1,4 @@
-<?phprequire('includes/student-header.php'); ?>
+<?php require('includes/student-header.php'); ?>
 <?php require('includes/sidebar.php'); ?>
 <?php 
 
@@ -7,25 +7,37 @@ require('includes/student-sessions.php');
 
 try {
       require('includes/connection.php');
-      $sql_query= " select * from studentInfo where studentID = '$student_id' ";
+      $sql_query= " select `studentInfo`.`studentID`, `college`.`collegeName`, `major`.`majorName`, `staff`.`fullName`, `studentInfo`.`passedCH`, `studentInfo`.`enrollmentStatus`, `studentInfo`.`academicStatus`, `studentInfo`.`CGPA`, `studentInfo`.`MCGPA`
+      FROM `studentInfo` 
+      LEFT JOIN `college` ON `studentInfo`.`collegeID` = `college`.`collegeID` 
+      LEFT JOIN `major` ON `major`.`collegeID` = `college`.`collegeID` 
+      LEFT JOIN `staff` ON `studentInfo`.`advisorID` = `staff`.`staffID`
+      WHERE studentID = '$student_id' ";
       $rs= $db->query($sql_query);
       $row= $rs->fetch();
 
-      $schedule_query= " SELECT studentClassroom.courseID, courseTiming.section, staff.fullName, courseTiming.lecturesDay,
-      courseTiming.lecturesTime, classroom.campus, classroom.room, course.credits, 
-      courseTiming.examDate, courseTiming.examTime, courseTiming.examPlace
-      FROM studentClassroom
-      LEFT JOIN staff ON studentClassroom.instructorID = staff.staffID
-      LEFT JOIN courseTiming ON courseTiming.instructorID = staff.staffID
-      LEFT JOIN classroom ON courseTiming.classroomID = classroom.classroomID
-      LEFT JOIN course ON courseTiming.courseID = course.courseID
-      WHERE studentClassroom.studentID= '$student_id' ";
+      $schedule_query= " select `studentClassroom`.`courseID`, `studentClassroom`.`section`, `staff`.`fullName`, `courseTiming`.`lecturesDay`, `courseTiming`.`lecturesTime`, `classroom`.`campus`, `classroom`.`room`, `course`.`credits`, `courseTiming`.`examDate`, `courseTiming`.`examTime`, `courseTiming`.`examPlace`
+      FROM `studentClassroom` 
+            LEFT JOIN `staff` ON `studentClassroom`.`instructorID` = `staff`.`staffID` 
+            LEFT JOIN `courseTiming` ON `courseTiming`.`instructorID` = `staff`.`staffID` AND courseTiming.section = studentClassroom.section
+            LEFT JOIN `classroom` ON `courseTiming`.`classroomID` = `classroom`.`classroomID` 
+            LEFT JOIN `course` ON `courseTiming`.`courseID` = `course`.`courseID`
+      WHERE studentClassroom.studentID= '$student_id' AND studentClassroom.year= 2023. AND studentClassroom.semester=2;";
 
       $schedule_rs= $db->query("$schedule_query");
+
+      // query to find registered CH
+      $registeredCH_sql_query="select SUM(`course`.`credits`)
+      FROM `studentClassroom` 
+      LEFT JOIN `course` ON `studentClassroom`.`courseID` = `course`.`courseID`
+      WHERE studentClassroom.studentID='$student_id' AND studentClassroom.year=2023 AND studentClassroom.semester=2";
+      $rigstered_rs= $db->query($registeredCH_sql_query);
+      $row_1= $rigstered_rs->fetch();
 
 }catch (PDOException $e){
       die("error: " . $e->getMessage());
 }
+
 
 ?>
 
@@ -37,9 +49,9 @@ try {
                         <h2>Course Schedule</h2>
                   </div>
                   <div class="inner-inner-div">
-                        <h2 style="text-transform: uppercase;"><?php echo $row['fullName']; ?></h2>
+                        <h2 style="text-transform: uppercase;"><?php echo $full_name; ?></h2>
                         <p style="">Student ID: <?php echo $row['studentID']; ?></p>
-                        <p style="margin-top:-0.4rem "><?php echo $row['major']; ?></p>
+                        <p style="margin-top:-0.4rem "><?php echo $row['majorName']; ?></p>
                         <div class="dashboard-text-container">
                               <div class="dashboard-text">
                                     <h3>Academic Status:</h3> <?php echo $row['academicStatus']; ?>
@@ -51,7 +63,7 @@ try {
                                     <h3>MCGPA:</h3> <?php echo $row['MCGPA']; ?>
                               </div>
                               <div class="dashboard-text">
-                                    <h3>Registered CH:</h3> not calculated yet
+                                    <h3>Registered CH:</h3> <?php echo $row_1[0]; ?>
                               </div>
                         </div>
                   </div>
