379623cc0ecf0ff4d97c3bec5a63023abc18faac
Ali-Alshaikh <alis3348s@gmail.com>
2023-05-28 17:08:29 +0300

    add student page validation done

--- admin/add-student.php
+++ admin/add-student.php
@@ -2,6 +2,7 @@
 require('../includes/header.php');
 require('../includes/admin-sidebar.php');
 require('../includes/admin-sessions.php');
+require('../includes/functions.php');
 try {
       require('../includes/connection.php');
 
@@ -24,6 +25,88 @@ try {
       $row= $next_id_rs->fetch();
 
       if (isset($_POST["add-student"])){
+
+            //validation here
+            //test_input function... 
+            $student_id= test_input($_POST["student-id"]);
+            $full_name= test_input($_POST["full-name"]);
+            $cpr= test_input($_POST["CPR"]);
+            $email= test_input($_POST["email"]);
+
+            $password= test_input($_POST["CPR"]);
+
+            if(empty($password)){
+
+                  die('Error: no input should be left empty!');
+            }
+
+            //password
+            //min pass word is 3 chars, max 15 chars
+            $pattPass = "/^[a-zA-Z0-9\@\#]{3,15}$/";
+            if(preg_match($pattPass,$password)!= 1){
+                  die('Error: please enter your password correctly ');
+            }
+
+            $hashed_password= password_hash($password, PASSWORD_DEFAULT);
+
+
+            $mobile_number= test_input($_POST["mobile-number"]);
+            $college= test_input($_POST["college"]);
+            $gender= test_input($_POST["gender"]);
+            $major= test_input($_POST["major"]);
+            $advisor= test_input($_POST["advisor"]);
+
+            //is empty
+            if(empty($student_id)||empty($full_name)||empty($cpr)||empty($email)||empty($mobile_number)||empty($college)||empty($gender)
+            ||empty($major)||empty($advisor)){
+                  die('Error: no input should be left empty!');
+            }
+
+
+            //regular expressions
+            
+            //student
+            //it must start with 20 , followed by 1 or 2 then any num 0-9, then 5 random digits, example: 20197180 , 202003838,etc..
+            $pattStu = "/^(20){1}([1-2]{1}[0-9]{1})\d{4,5}$/";
+            if(preg_match($pattStu,$student_id)!= 1){
+                  die('Error: please check your student id');
+            }
+
+            //full_name
+            //and capital or small letters will be accepted, maximum 80 chars
+            $pattFull = "/^[a-zA-Z\s]{3,80}$/";
+            if(preg_match($pattFull,$full_name)!= 1){
+                  die('Error: please enter your Full name correctly');
+            }
+
+            //CPR
+            //enter 9 digits, starts only from the 80's, any bahraini cpr will work: 010512345
+            $pattCpr = "/^([8-9][0-9]|0{1}[0-9]|1{1}[0-9]|[2][0-3])([0][1-9]|[1][0-2])\d{5}$/";
+            if(preg_match($pattCpr,$cpr)!= 1){
+                  die('Error: please enter your Cpr correctly');
+            }
+
+
+            //email
+            //any email will work: alis3348s@gmail.com, 20197180@stu.uob.edu.bh
+            $pattEmail = "/^[a-zA-Z0-9_-]+@[a-zA-Z0-9.]+$/";
+            if(preg_match($pattEmail,$email)!= 1){
+                  die('Error: please enter your email correctly');
+            }
+
+
+
+
+            //mobile number
+            //ex: 00973 33992900, 33992900 both accepted, must be 8 numbers to be exact
+            $pattMob = "/^((\+[0-9]{3}|00[0-9]{3})?)[0-9]{8}$/";
+            if(preg_match($pattMob,$mobile_number)!= 1){
+                  die('Error: please enter your mobile number correctly ');
+            }
+
+
+            //i did not remove this on purpose, see if you want to remove it:)
+            /*
             $student_id= $_POST["student-id"];
             $full_name= $_POST["full-name"];
             $cpr= $_POST["CPR"];
@@ -35,6 +118,13 @@ try {
             $gender= $_POST["gender"];
             $major= $_POST["major"];
             $advisor= $_POST["advisor"];
+            */
+
+            //----------------------------------------VALIDATION END--------------------------------------------------------------
+
+            //do we need to validate these? 
+
+
             if (empty($_POST['flat'])){
                   $flat= "null";
 
