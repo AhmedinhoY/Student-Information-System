87bf79d91ff1ef3b9714f7b11f095f37e21153a2
Ali-Alshaikh <alis3348s@gmail.com>
2023-05-29 04:05:12 +0300

    validation done

--- admin/add-staff.php
+++ admin/add-staff.php
@@ -5,6 +5,8 @@ require('../includes/admin-sessions.php');
 require('../includes/functions.php');
 try {
       require('../includes/connection.php');
+      $c=0;
+      $err="";
 
       //college selection
       $college_sql= "select collegeID, collegeName from college";
@@ -19,7 +21,8 @@ try {
             //password
             $password= test_input($_POST["CPR"]);
             if(empty($password)){
-                  die('<h1 style="text-align:center;">Error: no input must be left empty!</h1>');
+                  $err = '<h1 style="text-align:center;">Error: no input must be left empty!</h1>';
+                  $c++;
 
 
             }
@@ -28,21 +31,32 @@ try {
             //min pass word is 3 chars, max 15 chars
             $pattPass = "/^[a-zA-Z0-9\@\#]{3,15}$/";
             if(preg_match($pattPass,$password)!= 1){
-                  die('<h1 style="text-align:center;">Error: please enter your password correctly</h1>');
+                  $err ='<h1 style="text-align:center;">Error: please enter your password correctly</h1>';
+                  $c++;
+
 
             }
             $hashed_password= password_hash($password, PASSWORD_DEFAULT);
             //end password
 
             $mobile_number= test_input($_POST["mobile-number"]);//done
-            $college= test_input($_POST["college"]);//no need regular expression
-            $gender= test_input($_POST["gender"]);// no need regular expression
+
+            if(!empty($_POST["college"])){
+                  $college= test_input($_POST["college"]);//no need regular expression
+            }
+            if(!empty($_POST["gender"])){
+                  $gender= test_input($_POST["gender"]);// no need regular expression
+
+
+            }
             $job_title= test_input($_POST["job-title"]); //done 
 
             //empty? 
             if(empty($prefix)||empty($full_name1)||empty($email)||empty($cpr)||empty($mobile_number)||empty($college)||
             empty($gender)||empty($job_title)){
-                  die('<h1 style="text-align:center;">Error: no input must be left empty!</h1>');
+                  $err ='<h1 style="text-align:center;">Error: no input must be left empty!</h1>';
+                  $c++;
+
 
             }
 
@@ -50,7 +64,9 @@ try {
             // capital or small letters will be accepted with spaces , maximum 80 chars
             $pattFull = "/^[a-zA-Z\s]{3,80}$/";
             if(preg_match($pattFull,$full_name1)!= 1){
-                  die('<h1 style="text-align:center;">Error: please enter your Full name correctly</h1>');
+                  $err ='<h1 style="text-align:center;">Error: please enter your Full name correctly</h1>';
+                  $c++;
+
 
             }
 
@@ -60,7 +76,9 @@ try {
             //any email will work: alis3348s@gmail.com, 20197180@stu.uob.edu.bh
             $pattEmail = "/^[a-zA-Z0-9_-]+@[a-zA-Z0-9.]+$/";
             if(preg_match($pattEmail,$email)!= 1){
-                  die('<h1 style="text-align:center;">Error: please enter your email correctly</h1>');
+                  $err ='<h1 style="text-align:center;">Error: please enter your email correctly</h1>';
+                  $c++;
+
 
             }
 
@@ -69,7 +87,9 @@ try {
             //enter 9 digits, starts only from the 80's, any bahraini cpr will work: 010512345
             $pattCpr = "/^([8-9][0-9]|0{1}[0-9]|1{1}[0-9]|[2][0-3])([0][1-9]|[1][0-2])\d{5}$/";
             if(preg_match($pattCpr,$cpr)!= 1){
-                  die('<h1 style="text-align:center;">Error: please enter your Cpr correctly</h1>');
+                  $err ='<h1 style="text-align:center;">Error: please enter your Cpr correctly</h1>';
+                  $c++;
+
 
             }
 
@@ -77,22 +97,53 @@ try {
             //ex: 00973 33992900, 33992900 both accepted, must be 8 numbers to be exact
             $pattMob = "/^((\+[0-9]{3}|00[0-9]{3})?)[0-9]{8}$/";
             if(preg_match($pattMob,$mobile_number)!= 1){
-                  die('<h1 style="text-align:center;">Error: please enter your mobile number correctly</h1>');
+                  $err ='<h1 style="text-align:center;">Error: please enter your mobile number correctly</h1>';
+                  $c++;
+
 
             }
 
             //job_title
             $pattJob = "/^[a-zA-Z\s]{3,80}$/";
             if(preg_match($pattJob,$job_title)!= 1){
-                  die('<h1 style="text-align:center;">Error: Error: please enter your job title correctly </h1>');
+                  $err ='<h1 style="text-align:center;">Error: Error: please enter your job title correctly </h1>';
+                  $c++;
+
+
+            }
+
+            $sql11="select * from staff";
+            $s= $db->prepare($sql11); 
+            $s->execute();
+            $rows =$s->fetchAll(PDO::FETCH_NUM); 
+
+            foreach($rows as $row){
+
+                  if($row[3]==$email){
+                  $err ='<h1 style="text-align:center;">Error: this email exits</h1>';
+                  $c++;
+                  }
+
+                  if($row[7]==$mobile_number){
+                        $err ='<h1 style="text-align:center;">Error: this mobile number exits</h1>';
+                        $c++;
+                        }
+
+
+                  if($row[5]==$cpr){
+                        $err ='<h1 style="text-align:center;">Error: this CPR exits</h1>';
+                        $c++;
+                        }
+
+          
 
             }
 
             //office number ? --no need ? 
 
+            if($c==0){
 
-
-
+            
             // echo $email ;
             // echo $job_title ;            
             $insertion_query= "insert into staff (fullName, CPR, email, password, collegeID,
@@ -100,6 +151,7 @@ try {
             values('$full_name', '$cpr', '$email', '$hashed_password', '$college',
             '$gender', '$job_title', '$mobile_number')";
             $result = $db->exec($insertion_query);
+            }
       }
 
 } catch (PDOException $e){
@@ -113,7 +165,9 @@ die("error: " . $e->getMessage());
             <div class="form-element">
                   <div class="form-header">
                         <h2>Add Staff</h2>
+                        
                   </div>
+                  <h2>  <?php echo $err;?> </h2>
                   <div class="form-body">
                         <form action="" method="POST">
                               <div class="fields">
